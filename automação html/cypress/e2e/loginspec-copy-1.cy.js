ab26d77f8da79b5bc5a666a904099e7723c94b74
Bruna Florencio <jessicabruna901@gmail.com>
2025-07-01 09:35:30 -0300

    adding user data json

--- automação html/cypress/e2e/loginspec-copy-1.cy.js
+++ automação html/cypress/e2e/loginspec-copy-1.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/users/userData.jason'
+
 describe('orange HTM test', () => {
 
   const selectorList = {
@@ -5,23 +7,25 @@ describe('orange HTM test', () => {
     passwordFild:"[name='password']",
     loginButon: "[type='submit']",
     selectorTitleTopBar:'.oxd-topbar-header-breadcrumb-module',
+    dashbordGrid:".orangehrm-dashbord-grid",
     wrongCredentialAlert:"[role='alert']"
   }
 
+
   it('login - success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernamefield).type('Admin')
-    cy.get(selectorList.passwordFild).type('admin123')
+    cy.get(selectorList.usernamefield).type(userData.userSucess.username)
+    cy.get(selectorList.passwordFild).type(userData.userSucess.password)
     cy.get(selectorList.loginButon).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorList.selectorTitleTopBar).contanis('Dashbord')
   })
   it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernamefield).type('test')
+    cy.get(selectorList.usernamefield).type(userData.userSucess.password)
     cy.get(selectorList.passwordFild).type('test')
     cy.get(selectorList.loginButon).click()
-    cy.get(selectorList.selectorTitleTopBar)
+    cy.get(selectorList.dashbordGrid)
     
  })
 })
